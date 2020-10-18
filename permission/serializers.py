88e2bf9b2fdd81acdcb59fd48390438f12f68482
saihtunlu <saihtunlu14996@gmail.com>
2020-10-18 13:27:09 +0630

    first commit

---
+++ permission/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from account.serializers import UserSerializer
+from .models import Role, UserRole, Permission
+
+
+class PermissionSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Permission
+        fields = ['id', 'name', 'create', 'update', 'delete', 'read']
+
+
+class RoleSerializers(serializers.ModelSerializer):
+    permissions = PermissionSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = Role
+        fields = ['id', 'shop', 'name', 'permissions']
+
+
+class UserRoleSerializers(serializers.ModelSerializer):
+    role = RoleSerializers(many=False, read_only=True)
+
+    class Meta:
+        model = UserRole
+        fields = ['user', 'id', 'role']
